64774bb3b5ac2681541f54b9d6cd15566a42b097
zackarychapple <zackarychapple30@gmail.com>
2022-08-10 17:40:52 -0400

    adding simplified jenkins

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
-  console.log('testing again')
+  console.log('testing again again')
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
